c4bc131d696a8cb7492177822d17983f2af9d787
ericwindmill <ewindmill@greenbits.com>
2020-01-10 17:11:53 -0800

    init

---
+++ lib/flutter_by_example_server.dart
@@ -0,0 +1,11 @@
+/// flutter_by_example_server
+///
+/// A Aqueduct web server.
+library flutter_by_example_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
